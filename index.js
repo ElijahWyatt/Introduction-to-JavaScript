b3ad0fa8769f9503c47311f9536e8adabdf08f14
unknown <elijah.wyatt3@gmail.com>
2021-09-08 00:23:32 -0500

    answered 1d

--- index.js
+++ index.js
@@ -69,10 +69,10 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-  /*add your code here*/
-}
-
+function multiply(a,b){
+  let answer = a * b;
+  return answer;
+  }
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
